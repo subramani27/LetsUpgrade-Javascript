ca7a5ccc60dfa7906632a9bb1f2ee774e42be3de
subramani27 <61557799+subramani27@users.noreply.github.com>
2020-07-21 12:30:49 +0530

    Update and rename one.js to console.js
    Javascript code for types of console function

---
+++ assignment/day1/console.js
@@ -0,0 +1,99 @@
+console.log("hello world");
+
+
+console.warn("It is highly dangerous act!");
+
+/*console.count() -This method counts the number of times it has been called in the code.
+Any label can be inserted as a parameter.If not present ,the default keyword is logged into the screen*/
+
+console.count();
+console.count("countdown")
+
+//console.clear() -This method clears all the message in the screen
+console.clear()
+
+
+//console.group(  -The console.group() method indicates the start of a message group.
+
+console.log("Hello everybody");
+console.group();
+console.log("Hello And welcome to the new group");
+
+
+//console.groupEnd()  -to end a group
+
+console.groupEnd();
+
+//console.groupCollapsed() - this method indicates the start of a collapsed message group.
+
+console.log("Hello World");
+console.groupCollapsed();
+
+
+//console.info()  - writes a message to the console
+
+console.info("Hello India!")
+
+//console.time()  -starts a timer in the console view
+//console.timeEnd()  -ends the timer and shows the time taken to complete the operation
+
+console.time();
+for (i = 0; i < 100000; i++) {
+  // some code
+}
+console.timeEnd(); 
+
+
+//console.warn()  -writes a warning  message to the console
+
+console.warn("This is a highly vulnerable code");console.log("hello world");
+
+
+console.warn("It is highly dangerous act!");
+
+/*console.count() -This method counts the number of times it has been called in the code.
+Any label can be inserted as a parameter.If not present ,the default keyword is logged into the screen*/
+
+console.count();
+console.count("countdown")
+
+//console.clear() -This method clears all the message in the screen
+console.clear()
+
+
+//console.group(  -The console.group() method indicates the start of a message group.
+
+console.log("Hello everybody");
+console.group();
+console.log("Hello And welcome to the new group");
+
+
+//console.groupEnd()  -to end a group
+
+console.groupEnd();
+
+//console.groupCollapsed() - this method indicates the start of a collapsed message group.
+
+console.log("Hello World");
+console.groupCollapsed();
+
+
+//console.info()  - writes a message to the console
+
+console.info("Hello India!")
+
+//console.time()  -starts a timer in the console view
+//console.timeEnd()  -ends the timer and shows the time taken to complete the operation
+
+console.time();
+for (i = 0; i < 100000; i++) {
+  // some code
+}
+console.timeEnd(); 
+
+
+//console.warn()  -writes a warning  message to the console
+
+console.warn("This is a highly vulnerable code");
+
+
